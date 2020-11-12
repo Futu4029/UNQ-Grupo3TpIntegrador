4d33aa4592d820614f691fee6ae5b68757dc65db
gonzaloZarate <nofuture.gonza@gmail.com>
2020-11-11 23:46:48 -0300

    hotfix

--- Test/test/semZonaTest/PuntoDeVentaTest.java
+++ Test/test/semZonaTest/PuntoDeVentaTest.java
@@ -1,18 +1,13 @@
 package test.semZonaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import tpIntegrador.celular.SEMCelular;
-import tpIntegrador.semCompra.CompraPuntual;
 import tpIntegrador.semCompra.SEMCompra;
 import tpIntegrador.semEstacionamiento.SEMEstacionamiento;
 import tpIntegrador.semZona.PuntoDeVenta;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 class PuntoDeVentaTest {
@@ -22,26 +17,20 @@ class PuntoDeVentaTest {
 	private SEMCompra semCompra;
 	private SEMEstacionamiento semEstacionamiento;
 	private String patente;
-	private SEMCelular semCelular;
-	private CompraPuntual compra;
 	
 	@BeforeEach
 	public void setUp() {
-		
 		patente = "ABD123";
-		
-		compra = mock(CompraPuntual.class);
 		semCompra = mock(SEMCompra.class);
-		semCelular = mock(SEMCelular.class);
 		semEstacionamiento = mock(SEMEstacionamiento.class);
 		puntoDeVenta = new PuntoDeVenta(semCompra, semEstacionamiento);
 	}
 		
 	@Test
 	void generaCompraPuntual() {	
-		puntoDeVenta.generarCompraPuntual(5, "ABD123");
+		puntoDeVenta.generarCompraPuntual(5, patente);
 		verify(semCompra).generarCompraPuntual(5, puntoDeVenta);
-		verify(semEstacionamiento).generarEstacionamientoPuntual("ABD123", 5);
+		verify(semEstacionamiento).generarEstacionamientoPuntual(patente, 5);
 	}	
 	
 	@Test
